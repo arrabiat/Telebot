41e4d9d3c54de4782217db4eb4d2d14da8830859
Diego Licona <arrabiat@outlook.com>
2025-06-25 13:59:36 -0600

    First Version

---
+++ telebot_a.py
@@ -0,0 +1,18 @@
+import configparser
+import telebot
+"""
+Bot creado como prueba
+"""
+# Obtenemos credenciales de nuestro config.conf
+cp = configparser.ConfigParser()
+cp.read('./config.conf')
+bot = telebot.TeleBot(cp['AUTH']['bot_token'])
+
+@bot.message_handler(commands = ['start', 'help'])
+
+def cmd_start(message):
+    bot.reply_to(message, 'Hola, soy tu bot')
+    
+if __name__ == '__main__':
+    print('bot iniciado')
+    bot.infinity_polling()
